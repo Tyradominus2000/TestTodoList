d9282d1f756f7f63a227ffb037cda6d3a2f281c9
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-06-15 11:01:12 +0200

    feat : modify int test

--- TODO_FRONT/cypress/e2e/IntAppAddtodo.cy.js
+++ TODO_FRONT/cypress/e2e/IntAppAddtodo.cy.js
@@ -15,20 +15,17 @@ describe("Testing App with AddTodo", () => {
     // Click on the Add button
     cy.contains("Add").click();
     // Verify if the Todo is added
-    cy.get("ul > li > span").contains(MockText);
+    cy.get("ul > li").should("have.length", 1);
   });
   it("Not Adding Todo", () => {
     const MockText = "Test 2";
 
     // Select Input -> focus it -> clear the input -> Write the MockText -> Blur it
-    cy.get('input[placeholder="Add a todo"]')
-      .focus()
-      .clear()
-      .blur();
+    cy.get('input[placeholder="Add a todo"]').focus().clear().blur();
     // Click on the Add button
     cy.contains("Add").click();
 
     // Verify if the Todo is added
-    cy.get("ul > li > span").contains(MockText).should("not.exist");
+    cy.get("ul > li").should("have.length", 4);
   });
 });
