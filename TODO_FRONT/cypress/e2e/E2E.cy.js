91c330a671f9fe2f619ee2d8f3dd6d2180edbe0e
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-06-15 11:05:16 +0200

    feat : added comentary

--- TODO_FRONT/cypress/e2e/E2E.cy.js
+++ TODO_FRONT/cypress/e2e/E2E.cy.js
@@ -23,7 +23,7 @@ describe("template spec", () => {
   it("Not Adding Todo", () => {
     const MockText = "Test 2";
 
-    // Select Input -> focus it -> clear the input -> Write the MockText -> Blur it
+    // Select Input -> focus it -> clear the input -> Blur it
     cy.get('input[placeholder="Add a todo"]').focus().clear().blur();
     // Click on the Add button
     cy.contains("Add").click();
@@ -35,17 +35,23 @@ describe("template spec", () => {
   // Should modifie 4 todo
   it("Should modify", () => {
     const MockText = "Test 1 Modifier";
-
+    // Get the 4 button edit and click it
     cy.get("ul > li > div > button[data-testid=button-editTodo]").eq(3).click();
+    // Input MockText in the input
     cy.get('input[value="Test 1"]').focus().clear().type(MockText).blur();
+    // CLick on the save button
     cy.get("ul > div > button[data-testid=button-saveTodo").click();
+    // Verify if the value as been modified
     cy.get("ul > li > span").contains(MockText);
   });
 
+  // Delete the 4 todo
   it("Sould Delete", () => {
+    // Click on the delete button of the 4 todo
     cy.get("ul > li > div > button[data-testid=button-deleteTodo]")
       .eq(3)
       .click();
+    // Verify that their is only 3 element in the list so the 4 todo has been delete
     cy.get("ul[data-testid=ul] > li").should("have.length", 3);
   });
 });
--- TODO_FRONT/cypress/e2e/IntAppAddtodo.cy.js
+++ TODO_FRONT/cypress/e2e/IntAppAddtodo.cy.js
@@ -1,8 +1,10 @@
 describe("Testing App with AddTodo", () => {
+  // Visit the url
   beforeEach(() => {
     cy.visit("http://localhost:3000");
   });
 
+  // Test to add a todo
   it("Adding Todo", () => {
     const MockText = "Test 1";
 
@@ -18,14 +20,12 @@ describe("Testing App with AddTodo", () => {
     cy.get("ul > li").should("have.length", 1);
   });
   it("Not Adding Todo", () => {
-    const MockText = "Test 2";
-
     // Select Input -> focus it -> clear the input -> Write the MockText -> Blur it
     cy.get('input[placeholder="Add a todo"]').focus().clear().blur();
     // Click on the Add button
     cy.contains("Add").click();
 
-    // Verify if the Todo is added
+    // Verify if the Todo is not added
     cy.get("ul > li").should("have.length", 4);
   });
 });
